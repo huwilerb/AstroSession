aaf96b23cf7402ea3e97f412fd8efdec94781331
huwilerb <huwilerb@hotmail.ch>
2022-01-02 10:24:38 +0100

    Added methode description

--- image.py
+++ image.py
@@ -16,11 +16,34 @@ class Image():
         self.path = path 
         self.__exists()
     
-    def get_meta(self, save:bool=False)->dict:
+    def get_meta(self)->dict:
+        """
+        read the headers from a fits file
+
+        Parameters
+        ----------
+        file : .fits
+            a fits image
+        Returns
+        -------
+        h : dict
+            a dictionnary with the headers as key 
+
+        """
         return astro_tools.read_fits_header(self.path, restricted=Image.config.Restricted_headers)
         
         
     def populate_meta(self)->None:
+        """
+        populate the metadata in the object regarding to the image configuration file
+
+        Options
+        -------
+        the following options for the metadata in the object, to be changed in the configuration file:
+            Set a list of the headers as a new attribute : Headers_attribute_as_list == True
+            Set each header as a new attribute: Headers_as_ attributes == True 
+
+        """
         data = self.get_meta()
         if Image.config.Headers_attribute_as_list == True:
             self.headers = data
@@ -33,6 +56,9 @@ class Image():
         pass
 
     def __exists(self):
+        """
+        Check if an image exists
+        """
         if not astro_tools.check_path(self.path):
             print(f'The file {self.path} do not exists')
     
